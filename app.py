626acb03071db2c78662298620a4e960b37689d4
* <maoundis@gmail.com>
2021-02-20 07:12:58 +0800

    x

--- app.py
+++ app.py
@@ -7,9 +7,10 @@ app = Flask(__name__)
 def exec_():
   form = request.form
   if "code" in form and "py_v" in form:
+    print(code)
     code = base64.b64encode(form["code"].encode("utf-8")).decode()
     py_v = form["py_v"]
-
+    
     try:
       return s.check_output(['python%s' % py_v, '-c', 'exec(__import__("base64").b64decode("%s".encode()).decode())' % code, '2>', '/dev/null'])
     except Exception, e:
