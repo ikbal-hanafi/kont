21afe77022cfae416fe6a76a13ff5c9cf9d07a0d
* <maoundis@gmail.com>
2021-02-20 12:40:56 +0800

    x

--- app.py
+++ app.py
@@ -10,6 +10,6 @@ def exec_():
     code = base64.b64encode(form["code"].encode("utf-8")).decode()
     py_v = form["py_v"]
 
-    return s.get_output(f'python{py_v} -c \'exec(__import__("base64").b64decode("{code}".encode()).decode())\' 2> /dev/null')
+    return s.getoutput(f'python{py_v} -c \'exec(__import__("base64").b64decode("{code}".encode()).decode())\' 2> /dev/null')
 
   return abort(404)
