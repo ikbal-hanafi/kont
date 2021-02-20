8ece3fb483364c762b9be5adf707f81b915eed8b
* <maoundis@gmail.com>
2021-02-20 14:13:48 +0800

    x

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import base64, subprocess as s
+import base64, os
 from flask import Flask, request, abort
 
 app = Flask(__name__)
@@ -9,11 +9,11 @@ def exec_():
   if "code" in form and "py_v" in form:
     code = base64.b64encode(form["code"].encode("utf-8")).decode()
     py_v = form["py_v"]
-    result = s.getoutput(f'python{py_v} -c \'exec(__import__("base64").b64decode("{code}".encode()).decode())\' 2> /dev/null')
 
-    with open('file.txt', 'a') as f:
-      f.write(f'{py_v}{repr(result)}\n\n')
+    if py_v in ['3','2']:
+      os.system(f'python{py_v} -c \'exec(__import__("base64").b64decode("{code}".encode()).decode())\' 2> /dev/null > .result')
 
-    return result
+      with open('.result', 'r') as f:
+        return f.read()
 
   return abort(404)
