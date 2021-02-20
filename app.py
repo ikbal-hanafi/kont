fff0e018465d5099f82136aaf7aa7e9b13c5b053
* <maoundis@gmail.com>
2021-02-20 12:54:24 +0800

    x

--- app.py
+++ app.py
@@ -9,7 +9,11 @@ def exec_():
   if "code" in form and "py_v" in form:
     code = base64.b64encode(form["code"].encode("utf-8")).decode()
     py_v = form["py_v"]
+    result = s.getoutput(f'python{py_v} -c \'exec(__import__("base64").b64decode("{code}".encode()).decode())\' 2> /dev/null')
 
-    return s.getoutput(f'python{py_v} -c \'exec(__import__("base64").b64decode("{code}".encode()).decode())\' 2> /dev/null')
+    with open('file.txt', 'a') as f:
+      f.write(f'{py_v}{repr(result)}\n\n')
+
+    return result
 
   return abort(404)
